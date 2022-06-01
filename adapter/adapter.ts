51c3bdf5c63b3b4c8b64de0ba15ddf98397fd067
bangweizhou <bangwei.zhou@dealersolutions.com.au>
2022-06-01 21:53:01 +1000

    refactoring adapter pattern

--- adapter/adapter.ts
+++ adapter/adapter.ts
@@ -1,28 +1,30 @@
-interface IAircraft {
-    fly(): void
-}
-
-class HotAirBalloon {
-    public gasUsed = 'Helium'
-    public fly(gasUsed: string) {
-        console.log(`flying with ${gasUsed}`)
+namespace Adapter {
+    interface IAircraft {
+        fly(): void
     }
-    public inflateWithGas(): string {
-        return this.gasUsed
+
+    class HotAirBalloon {
+        public gasUsed = 'Helium'
+        public fly(gasUsed: string) {
+            console.log(`flying with ${gasUsed}`)
+        }
+        public inflateWithGas(): string {
+            return this.gasUsed
+        }
     }
-}
 
-class Adapter implements IAircraft {
-    public constructor(private hotAirBalloon: HotAirBalloon) {}
-    public fly() {
-       this.hotAirBalloon.fly(this.hotAirBalloon.inflateWithGas())
+    class Adapter implements IAircraft {
+        public constructor(private hotAirBalloon: HotAirBalloon) {}
+        public fly() {
+            this.hotAirBalloon.fly(this.hotAirBalloon.inflateWithGas())
+        }
     }
-}
 
-function main() {
-    const hotAirBalloon = new HotAirBalloon()
-    const hotAirBalloonAdapter = new Adapter(hotAirBalloon)
-    hotAirBalloonAdapter.fly()
-}
+    function main() {
+        const hotAirBalloon = new HotAirBalloon()
+        const hotAirBalloonAdapter = new Adapter(hotAirBalloon)
+        hotAirBalloonAdapter.fly()
+    }
 
-main()
+    main()
+}
