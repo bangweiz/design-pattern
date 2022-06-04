19f3402dc0072eed382e552485aee994ce7f464e
bangweizhou <bangwei.zhou@dealersolutions.com.au>
2022-06-04 13:13:17 +1000

    add command pattern

---
+++ command/command.ts
@@ -0,0 +1,71 @@
+namespace Command {
+    interface Command {
+        execute(): void
+    }
+
+    class SimpleCommand implements Command {
+        public constructor(private payload: string) {}
+
+        public execute() {
+            console.log(`This is a simple command with payload of ${this.payload}`)
+        }
+    }
+
+    class ComplexCommand implements Command {
+        public constructor(private receiver: Receiver, private a: string, private b: string) {}
+
+        public execute() {
+            console.log('ComplexCommand: Complex stuff should be done by a receiver object.');
+            this.receiver.doSomething(this.a);
+            this.receiver.doSomethingElse(this.b);
+        }
+    }
+
+    class Receiver {
+        public doSomething(a: string) {
+            console.log(`Receiver: Working on (${a})`)
+        }
+
+        public doSomethingElse(a: string) {
+            console.log(`Receiver: Working on (${a})`)
+        }
+    }
+
+    class Invoker {
+        private onStart: Command;
+        private onFinish: Command;
+
+        public setOnStart(command: Command): void {
+            this.onStart = command;
+        }
+
+        public setOnFinish(command: Command): void {
+            this.onFinish = command;
+        }
+
+        public doSomethingImportant(): void {
+            console.log('Invoker: Does anybody want something done before I begin?');
+            if (this.isCommand(this.onStart)) {
+                this.onStart.execute();
+            }
+
+            console.log('Invoker: ...doing something really important...');
+
+            console.log('Invoker: Does anybody want something done after I finish?');
+            if (this.isCommand(this.onFinish)) {
+                this.onFinish.execute();
+            }
+        }
+
+        private isCommand(object: any): object is Command {
+            return object.execute !== undefined;
+        }
+    }
+
+    const invoker = new Invoker();
+    invoker.setOnStart(new SimpleCommand('Say Hi!'));
+    const receiver = new Receiver();
+    invoker.setOnFinish(new ComplexCommand(receiver, 'Send email', 'Save report'));
+
+    invoker.doSomethingImportant();
+}
